0a90bba3c35fb30cf6bbb8e298b1783c8452f1d9
Raquel Santos <39015507+RaquelMSantos@users.noreply.github.com>
2026-05-24 15:47:19 -0300

    step 2: add layout

--- app/src/main/java/com/codesbyrachel/cafe/CafeScreen.kt
+++ app/src/main/java/com/codesbyrachel/cafe/CafeScreen.kt
@@ -1,24 +1,103 @@
 package com.codesbyrachel.cafe
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codesbyrachel.cafe.ui.theme.CafeTheme
+import com.codesbyrachel.cafe.ui.theme.SuccessGreen
 
 @Composable
 fun CafeScreen() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(text = "Olá!")
-        Text(text = "Bem-vinda ao Café")
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.cafe),
+            contentDescription = "Imagem de um café",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+        )
+
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(
+                text = "Cappuccino Especial",
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Text(
+                text = "R$ 12,90",
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(vertical = 4.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Café encorpado com leite vaporizado",
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Nome no copo", style = MaterialTheme.typography.labelSmall) },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Tamanho:",
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                Button(onClick = { }) { Text("P", style = MaterialTheme.typography.bodyLarge) }
+                Button(onClick = { }) { Text("M", style = MaterialTheme.typography.bodyLarge) }
+                Button(onClick = { }) { Text("G", style = MaterialTheme.typography.bodyLarge) }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = SuccessGreen
+                )
+            ) {
+                Text(
+                    text = "Fazer Pedido",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 }
 
